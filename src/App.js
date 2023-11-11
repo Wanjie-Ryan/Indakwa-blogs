ae115eacbbdc95da68e9d62820b66ee13cc46646
Ryan <ryanwanjiedistortion@gmail.com>
2023-11-11 12:09:03 +0300

    indakwa-blogs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginComponent from "./pages/login/login";
+import DashboardComponent from "./pages/dashboard/dash";
 
 function App() {
   return (
     <>
-      <LoginComponent />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/dashboard" element={<DashboardComponent />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/dashboard/dash.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function DashboardPages (){
+
+    return(
+
+        <>
+
+
+        </>
+    )
+}
+
+export default DashboardPages
