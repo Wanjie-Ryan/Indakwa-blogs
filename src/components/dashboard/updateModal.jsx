cf42d77efb392f57c5971104ef6d0059f31af024
Ryan <ryanwanjiedistortion@gmail.com>
2023-11-20 07:09:43 +0300

    blog-admin

--- src/components/dashboard/updateModal.jsx
+++ src/components/dashboard/updateModal.jsx
@@ -7,9 +7,8 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function Checkout({ isOpen, onClose,selectedItem }) {
-
-    console.log(selectedItem)
+function Checkout({ isOpen, onClose, selectedItem }) {
+//   console.log(selectedItem);
 
   const [image, setImage] = useState();
   const [name, setName] = useState();
@@ -30,12 +29,63 @@ function Checkout({ isOpen, onClose,selectedItem }) {
 
   const [loading, setLoading] = useState(false);
 
-  const updateBlog = async(e)=>{
-    
-  }
+  const updateBlog = async (e) => {
+    e.preventDefault();
 
+    setLoading(true);
 
+    try {
+      if (!image) {
+        toast.error("Please ul=pload an image");
 
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("file", image);
+
+      formData.append("upload_preset", "pq4z6rjr");
+
+      const imageData = await axios.post(
+        "https://api.cloudinary.com/v1_1/djgk2k4sw/image/upload",
+        formData
+      );
+
+    //   console.log(imageData);
+
+      const blogData = {
+        image: imageData.data.secure_url,
+        name: name,
+        description: description,
+        link: link,
+      };
+
+      const response = await axios.patch(
+        `https://informed-perspective.onrender.com/api/blogs/updateblog/${selectedItem}`,
+        blogData
+      );
+
+    //   console.log(response);
+
+      toast.success("Blog updated Successfully");
+
+      setLoading(false);
+
+      setTimeout(() => {
+        window.location.reload();
+      }, 2000);
+    } catch (err) {
+      if (err.response.status === 500) {
+        toast.error("A problem with our servers, hang on");
+      } else if (err.response.status === 400) {
+        toast.error("Bad request");
+      } else if (err.response.status === 404) {
+        toast.error("No Blog Found");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -60,18 +110,28 @@ function Checkout({ isOpen, onClose,selectedItem }) {
             </div>
           </nav>
 
-          <form className="contact__contact-container">
+          <form className="contact__contact-container" onSubmit={updateBlog}>
             <div className="contact__contact-container__name">
               <label>Blog Image:</label>
               <input
                 type="file"
                 accept="image/*"
+                required
                 placeholder="enter the image of the blog"
+                onChange={(e) => {
+                  setImage(e.target.files[0]);
+                }}
               />
             </div>
             <div className="contact__contact-container__name">
               <label>Name</label>
-              <input type="text" required placeholder="enter the blog name" />
+              <input
+                type="text"
+                required
+                placeholder="enter the blog name"
+                value={name}
+                onChange={handleName}
+              />
             </div>
 
             <div className="contact__contact-container__name">
@@ -80,6 +140,8 @@ function Checkout({ isOpen, onClose,selectedItem }) {
                 type="text"
                 required
                 placeholder="enter blog description"
+                value={description}
+                onChange={handleDescription}
               />
             </div>
 
@@ -89,10 +151,18 @@ function Checkout({ isOpen, onClose,selectedItem }) {
                 type="text"
                 placeholder="enter the link of the blog"
                 required
+                value={link}
+                onChange={handleLink}
               />
             </div>
 
-            <button className="contact__okay-btn">Okay</button>
+            <button className="contact__okay-btn">
+              {loading ? (
+                <AiOutlineLoading3Quarters className="loading-icon" />
+              ) : (
+                "Okay"
+              )}
+            </button>
           </form>
         </main>
       </Modal>
